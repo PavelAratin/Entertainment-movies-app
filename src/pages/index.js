de7bd193c0061b55ea77075f9a1409d565474797
Pavel <paha_gagarin@mail.ru>
2023-04-10 19:36:13 +0300

    добавление формы поиска

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,9 +1,13 @@
+import Form from "@/components/Form";
 import Header from "@/components/Header";
 
 export default function Home() {
   return (
     <div className="site-container">
       <Header></Header>
+      <main>
+        <Form></Form>
+      </main>
     </div>
   )
 }
