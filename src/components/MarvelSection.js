b2620b2a58235eda4a51171cc7c8fde210e5ac9f
Pavel <paha_gagarin@mail.ru>
2023-04-25 09:58:42 +0300

    добавление логики отслеживания ширины экрана для адаптивности слайдера

--- src/components/MarvelSection.js
+++ src/components/MarvelSection.js
@@ -3,16 +3,29 @@ import { Swiper, SwiperSlide } from 'swiper/react';
 import CardMarvel from './CradMarvel';
 // Import Swiper styles
 import 'swiper/css';
+import { useEffect, useState } from 'react';
 
 const MarvelSection = ({ dataMarvelCharacters }) => {
+  const [windowWidth, setWindowWidth] = useState(undefined)
+
+  useEffect(() => {
+    const changeWindowWidth = () => {
+      setWindowWidth(window.innerWidth);
+      console.log(window.innerWidth)
+    };
+    window.addEventListener('resize', changeWindowWidth)
+    changeWindowWidth();
+    //перестаем считать ширину окна, при размонтировании компонента
+    return () => window.removeEventListener('resize', changeWindowWidth)
+  }, [])
   return (
     <section className='trending'>
       <h2 className='title-h2'>Marvel movie</h2>
       <Swiper
         spaceBetween={20}
-        slidesPerView={5.5}
-        // onSlideChange={() => console.log('slide change')}
-        // onSwiper={(swiper) => console.log(swiper)}
+        slidesPerView={windowWidth >= 1250 ? 5.5 : 2.5}
+      // onSlideChange={() => console.log('slide change')}
+      // onSwiper={(swiper) => console.log(swiper)}
       >
         {dataMarvelCharacters.data.results.map((marvelCharacter) => (
           <SwiperSlide key={marvelCharacter.id}>
